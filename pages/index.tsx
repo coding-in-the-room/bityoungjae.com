2a1101b97cb786a1d3eba91aea26d27f35e412c4
Youngjae Joo <bityoungjae@gmail.com>
2020-06-24 03:15:57 +0900

    feat(pages/index.tsx): mETADATA 로드 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import METADATA from '../metadata.json';
+import Head from 'next/head';
 
 const IndexPage: React.FC = () => {
-  return <div>Hello World!</div>;
+  const { title, description } = METADATA;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </>
+  );
 };
 
 export default IndexPage;
