eaba3a770e3d065edef4016471a5bba5fefd1358
Youngjae Joo <bityoungjae@gmail.com>
2020-06-24 02:47:38 +0900

    init: 블로그 프로젝트 기본 골격 생성

---
+++ pages/categories/[...slug].tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+const CategoryPage: React.FC = () => <div>Post 페이지에용</div>;
+
+export default CategoryPage;
+
+/*
+Server-side Block 입니다
+*/
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
