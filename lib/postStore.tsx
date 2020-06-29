fa6a41e254552373822601cf7766a06095a99cd8
Youngjae Joo <bityoungjae@gmail.com>
2020-06-29 21:38:57 +0900

    feat(poststore): postStore 기본 구현 완료

---
+++ lib/postStore.tsx
@@ -0,0 +1,108 @@
+import type { PostData } from './postParser';
+import { findNode, findNodeAll, visit } from './visit';
+import { getNodeTree, FileNode } from './getNodeTree';
+
+interface Path {
+  params: {
+    [paramName: string]: string;
+  };
+}
+
+interface PostStoreProps {
+  rootDir: string;
+}
+
+const convertToPath = (paramName: string) => (slug: string) => ({
+  params: {
+    [paramName]: slug,
+  },
+});
+
+interface PostStore {
+  rootDir: string;
+  rootNode: FileNode;
+  postNodes: FileNode[];
+  posts: PostData[];
+  postPathList: Path[];
+  shouldUpdate: boolean;
+}
+
+const store: Partial<PostStore> = {
+  shouldUpdate: true,
+  postNodes: [],
+};
+
+export const getStore = async (options: PostStoreProps) => {
+  if (!store.shouldUpdate) return store;
+
+  const { rootDir } = options;
+  store.rootDir = rootDir;
+
+  store.rootNode = await getNodeTree({
+    nodePath: rootDir,
+    postStore: store.postNodes,
+  });
+
+  store.posts = store.postNodes.map((node) => node.postData);
+
+  store.postPathList = store.posts.map(({ slug }) =>
+    convertToPath('slug')(slug),
+  );
+
+  store.shouldUpdate = false;
+
+  return store;
+};
+
+const isCategory = (node: FileNode) => node.type === 'category';
+const isPost = (node: FileNode) => node.type === 'post';
+
+export const getCategoryBySlug = (slug: string) => {
+  return findNode(
+    store.rootNode,
+    (node) => isCategory(node) && node.slug === slug,
+  );
+};
+
+export const getPostBySlug = (rootNode: FileNode, slug: string): FileNode => {
+  return findNode(rootNode, (node) => node.slug === slug);
+};
+
+export const getPostsByCategories = (
+  rootNode: FileNode,
+  categories: string[],
+) => {
+  const categoryNode = findCategory(rootNode, categories);
+  const posts = findNodeAll(categoryNode, isPost);
+
+  return posts;
+};
+
+const findCategory = (rootNode: FileNode, categories: string[]) => {
+  let now: FileNode = rootNode;
+
+  for (const category of categories) {
+    now = findNode(now, (node) => isCategory(node) && node.slug === category);
+  }
+
+  return now;
+};
+
+// 디렉터리 계층구조 중 next.js에 사용될 카테고리 path만 추출.
+export const getCategoriesPaths = (
+  rootNode: FileNode,
+  parents: string[] = [],
+) => {
+  const result: any = [];
+
+  if (parents.length) result.push(parents);
+
+  for (const node of rootNode.children) {
+    if (isCategory(node)) {
+      const category = [...parents, node.slug];
+      result.push(...getCategoriesPaths(node, category));
+    }
+  }
+
+  return result;
+};
---
+++ tests/postStore.test.ts
@@ -0,0 +1,39 @@
+import {
+  getStore,
+  getCategoriesPaths,
+  getPostsByCategories,
+  getPostBySlug,
+} from '../lib/postStore';
+import isEqual from 'lodash.isequal';
+import { testPath } from './env';
+
+test('getCategoriesPaths', async () => {
+  const store = await getStore({ rootDir: testPath });
+  const categories = getCategoriesPaths(store.rootNode);
+
+  const target = [
+    ['javascript'],
+    ['javascript', '특별-시리즈'],
+    ['react'],
+    ['redux'],
+  ];
+
+  expect(isEqual(target, categories)).toBe(true);
+});
+
+test('getPostsByCategories', async () => {
+  const store = await getStore({ rootDir: testPath });
+  const posts = getPostsByCategories(store.rootNode, [
+    'javascript',
+    '특별-시리즈',
+  ]);
+
+  expect(posts[0].slug).toBe('자바스크립트의-모든-것-1탄');
+});
+
+test('getPostsBySlug', async () => {
+  const store = await getStore({ rootDir: testPath });
+  const post = getPostBySlug(store.rootNode, '자바스크립트의-모든-것-1탄');
+
+  expect(post?.postData?.title).toBe('자바스크립트의 모든 거엇!');
+});
